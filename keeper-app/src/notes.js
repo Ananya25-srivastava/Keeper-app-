06de3d0d3c240dc63c4b89a4fce1a9d3aec00d1d
Ananya Srivastava <72756749+Ananya25-srivastava@users.noreply.github.com>
2024-02-05 22:34:09 +0530

    Add files via upload

---
+++ keeper-app/src/notes.js
@@ -0,0 +1,22 @@
+const notes = [
+  {
+    key: "1",
+    title: "Note 1",
+    content: "Content 1",
+  },
+  {
+    key: "2",
+    title: "Note 2",
+    content: "Content 2",
+  },
+  {
+    key: "3",
+    title: "Note 3",
+    content: "Content 3",
+  },
+  {
+    key: "3",
+    title: "Note 3",
+    content: "Content 3",
+  },
+];
